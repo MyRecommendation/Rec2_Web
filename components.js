1adb8a719ac572269d3b7295dc4d3efd8aa40c13
dattang12 <dattang1201@gmail.com>
2026-06-25 16:19:16 -0700

    add google analytics4 tracking

--- components.js
+++ components.js
@@ -1,3 +1,15 @@
+// Google Analytics 4
+(function() {
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=G-9Q5V3WNLRG';
+  document.head.appendChild(s);
+})();
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-9Q5V3WNLRG');
+
 (function () {
   const inSubdir = window.location.pathname.includes('/services/');
   const b = inSubdir ? '../' : '';
